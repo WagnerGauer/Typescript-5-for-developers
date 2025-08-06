74cd3be2f42fb22e43dfed55cc815ca5ac440a3c
Wagner Gauer <wggauer@gmail.com>
2025-08-06 18:26:06 -0300

    learn how to compile typescript code which is by using tsc  --init to create a configuration file, in the configuration file I can adjust many things such as the root and output directories

---
+++ dist/First.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=First.d.ts.map
---
+++ dist/First.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+async function printName(arg) {
+    console.log(arg.name);
+}
+printName({ name: "John" });
+//# sourceMappingURL=First.js.map
