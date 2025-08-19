4b3780f58009a917e0b5e12e655690db05a202e7
Wagner Gauer <wggauer@gmail.com>
2025-08-19 17:13:30 -0300

    learn about extended classes and how to override a method from an extended clss

---
+++ section4/src/3ClassExtensions.ts
@@ -0,0 +1,33 @@
+class Project {
+  name: string;
+  budget: number;
+
+  constructor(name: string, budget: number) {
+    this.name = name;
+    this.budget = budget;
+  }
+
+  printBudget() {
+    console.log(`${this.name} has a budget of ${this.budget}`);
+  }
+}
+
+class SecretProject extends Project {
+  secrecyLevel: 1 | 2 | 3;
+
+  constructor(secrecyLevel: 1 | 2 | 3) {
+    // this super line down here is basically a call to the base class, so in this case the Project class
+    super("Secret project", 1000000);
+    this.secrecyLevel = secrecyLevel;
+  }
+
+  // What this line here is overriding the method from the extended class so that
+  // the buget is not displayed
+  override printBudget() {
+    console.log(`The budget is secret!`);
+  }
+}
+
+const secretProject = new SecretProject(3);
+
+secretProject.printBudget();
